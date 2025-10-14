6d0a62ee66d3ec12aa74fc918e8918b83251d661
ibhatnasir <ibhatnasir@gmail.com>
2025-10-14 11:15:29 +0530

    adding 1 to 5 in for loop:

--- script.js
+++ script.js
@@ -14,3 +14,12 @@ for(let i=1;i<=5;i++){
 }
 console.log("Loop has ended");
 
+// Calculate sum of 1 to 5 
+
+let sum =0;
+for(let i=1; i<=5; i++){
+    sum = sum + i;
+}
+console.log("sum =", sum);
+
+
