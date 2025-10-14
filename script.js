516cb31d37443805e4040f9d0cf048701de99254
ibhatnasir <ibhatnasir@gmail.com>
2025-10-14 11:03:46 +0530

    first for loop:

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ console.log("a+b=",a+b);
 console.log("a*b=",a*b);
 console.log("a-b=",a-b);
 console.log("a/b=",a/b);
+
+// Loops in JS
+
+for(let i=1;i<=5;i++){
+    console.log("Hello",i);
+}
+console.log("Loop has ended");
+
